d86226e5a76e87d574638dbe9c95768d7ce454ab
Sean Park <sean@synmbp13.local>
2020-10-24 15:53:50 +0900

    models.py modify and python manage.py makemigrations

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# 모델 : 데이터베이스를 SQL없이 다루려고 모델을 사용
+# 우리가 데이터를 객체화해서 다루겠다.
+# 모델 = 테이블
+# 모델의 필드 = 테이블의 컬럼
+# 인스턴스 = 테이블의 레코드
+# 필드의 가뵤(인스턴스의 필드값) = 레코드의 컬럼 데이터 값
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+    # 필드의 종류가 결정하는 것
+    # 1. 데이터베이스의 컬럼 종류
+    # 2. 제약사항 (몇글자까지)
+    # 3. From의 종류
+    # 4. From에서 제약사항
+
+# 모델을 만들었다 => 데이터베이스에 어떤 데이터들을 어던 형태로 넣을지 결정!
+# makemigartions => 모델의 변경사항을 추적해서 기록
+# 마이그레이션(migate) => 데이터베이스에 모델의 내용을 반용(테이블 생성)
